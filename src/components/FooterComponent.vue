aad4cf3bdc0fd152b1fbc24fbf86be9dc3288c26
KLAIIIII <guyyg03546@gmail.com>
2022-11-03 15:41:52 +0700

    Update

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,13 +1,13 @@
 <template lang="en">
-  <div>
-    <h1>About</h1>
-  </div>
+    <div>
+        <h1>Footer</h1>
+    </div>
 </template>
 <script>
 export default {
-  
+    
 }
 </script>
 <style lang="">
-  
+    
 </style>
--- src/components/MenuComponent.vue
+++ src/components/MenuComponent.vue
@@ -1,13 +1,15 @@
 <template lang="en">
-  <div>
-    <h1>About</h1>
-  </div>
+    <div>
+        <h1>Menu</h1>
+    </div>
 </template>
+
 <script>
 export default {
-  
+    
 }
 </script>
+
 <style lang="">
-  
+    
 </style>
